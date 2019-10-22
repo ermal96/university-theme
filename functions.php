100bb124bb1fcb9c56214d58cdf020aa50c40272
ermal96 <ermalvrapi18@gmail.com>
2019-10-22 12:38:59 +0200

    Blog right sidebar

--- functions.php
+++ functions.php
@@ -19,3 +19,23 @@ function university_features() {
 }
 
 add_action('after_setup_theme', 'university_features');
+
+
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Blog right sidebar',
+		'id'            => 'blog_right_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
