42b7f5d51e215aa14a439ea80937a72b9163850c
ermal96 <ermalvrapi18@gmail.com>
2019-10-22 09:53:42 +0200

    added wp post-thumbnails support

--- functions.php
+++ functions.php
@@ -14,6 +14,7 @@ function university_features() {
   register_nav_menu('footerMenuLocationOne', 'Footer Menu Location One ');
   register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two ');
   add_theme_support('title-tag');
+  add_theme_support( 'post-thumbnails' ); 
 }
 
 add_action('after_setup_theme', 'university_features');
