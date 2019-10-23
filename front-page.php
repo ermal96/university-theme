34d99b514f045809d1c6ba5c984614e461f6445b
ermal96 <ermalvrapi18@gmail.com>
2019-10-23 14:13:51 +0200

    #

--- front-page.php
+++ front-page.php
@@ -4,8 +4,8 @@
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
     <div class="page-banner__content container t-center c-white">
       <h1 class="headline headline--large">Welcome!</h1>
-      <h2 class="headline headline--medium">We thinkkkkkk you&rsquo;ll like it here.</h2>
-      <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+      <h2 class="headline headline--medium">We think you’ll like it here.</h2>
+      <h3 class="headline headline--small">Why don’t you check out the major you’re interested in?</h3>
       <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
     </div>
   </div>
