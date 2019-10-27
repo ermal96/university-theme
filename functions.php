3ec3695855e30e4020607161b8ac66938d41aa52
ermal96 <ermalvrapi18@gmail.com>
2019-10-27 17:13:24 +0100

    Menu

--- functions.php
+++ functions.php
@@ -19,8 +19,8 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features()
 {
-    register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    register_nav_menu('footerMenuLocationOne', 'Footer Menu Location One ');
+    register_nav_menu('headerMenuLocation', 'Primary');
+    register_nav_menu('footerMenuLocationOne', 'Footer ');
     register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two ');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
