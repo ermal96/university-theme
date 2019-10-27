cb80358a68061e3ea050554a261f38e616b4214c
ermal96 <ermalvrapi18@gmail.com>
2019-10-27 23:25:28 +0100

    page banner

--- functions.php
+++ functions.php
@@ -31,20 +31,22 @@ function university_features()
 add_action('after_setup_theme', 'university_features');
 
 
-function pageBanner()
+function pageBanner($args = null)
 {
-    ?>
+    $subtitle = get_field('page_banner_subtitle');
+    $backImg = get_field('page_banner_background_image');
+    $title = get_the_title(); ?>
 
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php $backImg = get_field('page_banner_background_image');
+  <div class="page-banner__bg-image" style="background-image: url(<?php
     echo $backImg['url']
     ?>);">
   </div>
   <div class="page-banner__content container container--narrow">
-    <h2 class="page-banner__title"><?php the_title(); ?>
+    <h2 class="page-banner__title"><?php echo $title ?>
     </h2>
     <div class="page-banner__intro">
-      <p><?php the_field('page_banner_subtitle'); ?>
+      <p><?php echo $subtitle ?>
       </p>
     </div>
   </div>
