25409df33e2f1b79ab9c1c59daa7f970cb51ad65
ermal96 <ermalvrapi18@gmail.com>
2019-10-28 17:31:46 +0100

    search overlay

--- footer.php
+++ footer.php
@@ -56,7 +56,14 @@
 
   </div>
 </footer>
-
+<div class="search-overlay">
+      <div class="search-overlay__top">
+      <div class="container">
+      <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+      <input placeholder="what are you looking for ?" type="text" name="" id="seach-tearm" class="search-term">
+      <i class="fa fa-close search-overlay__close" aria-hidden="true"></i>
+      </div>
+</div>
 <?php wp_footer(); ?>
 </body>
 
