571036f1e3bec7c2bebea10a4500aea4d971f587
ermal96 <ermalvrapi18@gmail.com>
2019-10-28 17:32:00 +0100

    include jquery

--- inc/include.php
+++ inc/include.php
@@ -4,8 +4,11 @@
 
 function university_files()
 {
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
     wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyAkuRL11s7kwmLpyEr5hnQqnphLAo8n8_w', null, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0', true);
+    wp_enqueue_script('customJs', get_theme_file_uri('/js/custom.js'), NULL, '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Noto+Sans+JP:300,400,500');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
@@ -13,3 +16,5 @@ function university_files()
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
+
+
