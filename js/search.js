4f8f176d11c795e8629242c1af1a8e6d9c7ba8d3
ermal96 <ermalvrapi18@gmail.com>
2019-10-30 15:59:34 +0100

    #

--- js/search.js
+++ js/search.js
@@ -149,13 +149,12 @@ class Search {
             <div class="search-overlay__top">
                 <div class="container">
                     <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-                        <input placeholder="what are you looking for ?" type="text" name="" id="search-term" class="search-term">
+                    <input placeholder="what are you looking for ?" type="text" name="" id="search-term" class="search-term">
                     <i class="fa fa-close search-overlay__close" aria-hidden="true"></i>
                 </div>
             </div>
             <div class="container">
-                <div id="search-overlay__results">
-                </div>
+                <div id="search-overlay__results"></div>
             </div>
             </div>
         `);
