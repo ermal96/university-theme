b10f1ecb5d19e62698cf2ac52525f2183b3da76a
ermal96 <ermalvrapi18@gmail.com>
2019-10-31 10:15:20 +0100

    removed google map api from functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,4 @@
 <?php
 
-function register_map_key($api){
-
-    $api['key'] = 'AIzaSyAkuRL11s7kwmLpyEr5hnQqnphLAo8n8_w';
-    return $api;
-}
-
-
-add_filter('acf/fields/google_map/api', 'register_map_key');
-
-
 
 require(get_template_directory() . '/inc/init.php');
