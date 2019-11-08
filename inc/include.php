9c01f2db657f5983c28e6cc743578d08154ed29b
ermal96 <ermalvrapi18@gmail.com>
2019-11-08 11:24:38 +0100

    included like js file

--- inc/include.php
+++ inc/include.php
@@ -8,9 +8,10 @@ function university_files()
     wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
     wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyAkuRL11s7kwmLpyEr5hnQqnphLAo8n8_w', null, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0', true);
-    wp_enqueue_script('searchJs', get_theme_file_uri('/js/search.js'), NULL, '1.0', true);
-    wp_enqueue_script('myNotes', get_theme_file_uri('/js/myNotes.js'), NULL, '1.0', true);
-    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Noto+Sans+JP:300,400,500');
+    wp_enqueue_script('searchJs', get_theme_file_uri('/js/search.js'), null, '1.0', true);
+    wp_enqueue_script('myNotes', get_theme_file_uri('/js/myNotes.js'), null, '1.0', true);
+    wp_enqueue_script('like', get_theme_file_uri('/js/like.js'), null, '1.0', true);
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Noto+Sans+JP:400');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());
@@ -21,5 +22,3 @@ function university_files()
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
-
-
